972bd8c7559f3dc6bbfd78473bb225b18f8897a1
Ram Jeeva Sajan I <88493043+Sajan-Ilayaraja@users.noreply.github.com>
2025-10-04 18:06:10 +0530

    Delete lucide-react.d.ts

--- lucide-react.d.ts
+++
@@ -1,8 +0,0 @@
-import type { LucideProps } from 'lucide-react';
-import type { ForwardRefExoticComponent, RefAttributes } from 'react';
-
-declare module 'lucide-react' {
-  export const Wind: ForwardRefExoticComponent<
-    Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
-  >;
-}
