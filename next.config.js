c92b9c3d6b2c5fce4d5bedfe4d60e830b4da0405
Ram Jeeva Sajan I <88493043+Sajan-Ilayaraja@users.noreply.github.com>
2025-10-05 00:12:20 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Enable static export
-    images: {
-        unoptimized: true
-    },
     webpack: (config) => {
       // Ignore Node.js-specific modules in frontend
       config.resolve.fallback = {
@@ -12,15 +9,15 @@ const nextConfig = {
         path: false,
         os: false,
       };
-
+  
       // Suppress Handlebars require.extensions warning
       config.module.rules.push({
         test: /handlebars\/lib\/index\.js$/,
         use: 'null-loader',
       });
-
+  
       return config;
     },
   };
-
+  
   module.exports = nextConfig;
